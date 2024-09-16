c6fce5619cf8260aa001c950f5818db4841ea536
Riocodex <onwukachibike@gmail.com>
2024-09-16 11:47:12 +0200

    updated

--- assets/components/ProductDisplay.js
+++ assets/components/ProductDisplay.js
@@ -1,6 +1,5 @@
 app.component('product-display',{
     template:
-    /*html*/
     ` <div class="product-display">
     <div class="product-container">
       <div class="product-image">
--- main.js
+++ main.js
@@ -2,33 +2,11 @@ const app = Vue.createApp({
     data() {
         return {
             cart: 0,
-            product: 'Socks',
-            brand: 'You Sock!',
-            selectedVariant: 0, // Tracks the current variant
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 ,onSale: true},
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 10 ,onSale: false},
-            ],
+            
         }
     },
     methods: {
-        addToCart() {
-            this.cart += 1;
-        },
-        updateVariant(index) {
-            this.selectedVariant = index;
-        },
+     
     },
-    computed: {
-        title() {
-            return this.brand + ' ' + this.product;
-        },
-        image() {
-            return this.variants[this.selectedVariant].image;
-        },
-        inStock() {
-            return this.variants[this.selectedVariant].quantity > 0;
-        },
-    }
+  
 });
