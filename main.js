c93f34615dd7bab7fa3408f524661b5efafd3a71
Riocodex <onwukachibike@gmail.com>
2024-09-16 11:23:33 +0200

    updated

--- main.js
+++ main.js
@@ -1,27 +1,34 @@
 const app = Vue.createApp({
-    data(){
-        return{
-            cart:0,
-            product:'Socks',
-            image: './assets/images/socks_green.jpg',
-            inStock: true,
-            details:['50% cotton', '30% wool', '20% polyester'],
-            variants:[
-                { id:2234, color:'green', image:"./assets/images/socks_green.jpg"},
-                { id:2235, color:"blue", image:"./assets/images/socks_blue.jpg"},
-            ]
+    data() {
+        return {
+            cart: 0,
+            product: 'Socks',
+            brand: 'You Sock!',
+            selectedVariant: 0, // Tracks the current variant
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 ,onSale: true},
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 10 ,onSale: false},
+            ],
         }
     },
     methods: {
-        addToCart(){
-            this.cart +=1
+        addToCart() {
+            this.cart += 1;
         },
-        removeFromCart(){
-            this.cart-=1
+        updateVariant(index) {
+            this.selectedVariant = index;
         },
-
-        updateImage(variantImage){
-            this.image = variantImage
-        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].image;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity > 0;
+        },
     }
-})
+});
